42c13231c9ddb60af5c4da3d57bdd599f9456d52
Fabian Troxler <fabian.troxler@gmail.com>
2013-09-12 16:47:33 +0200

    added timestamp and primdocs

---
+++ AtlasTiXMLImporter/src/ch/fabiantroxler/atlastixmlimporter/DataSource.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.fabiantroxler.atlastixmlimporter;
+
+/**
+ *
+ * @author fabiantroxler
+ */
+public class DataSource {
+    
+    public String id = "";
+    public String loc = "";
+    public String mime = "";
+    public String tf = "";
+    public int mfps = 0;
+    
+}
--- AtlasTiXMLImporter/src/ch/fabiantroxler/atlastixmlimporter/ImporterAtlasTiXML.java
+++ AtlasTiXMLImporter/src/ch/fabiantroxler/atlastixmlimporter/ImporterAtlasTiXML.java
@@ -27,6 +27,9 @@ import org.gephi.utils.progress.Progress;
 import org.gephi.utils.progress.ProgressTicket;
 import org.openide.util.NbBundle;
 
+import ch.fabiantroxler.atlastixmlimporter.DataSource;
+import java.util.ArrayList;
+
 /**
  *
  * @author fabiantroxler
@@ -34,10 +37,24 @@ import org.openide.util.NbBundle;
 
 
 public class ImporterAtlasTiXML implements FileImporter, LongTask{
+   
+   private ArrayList<DataSource> dataSourceList = new ArrayList<DataSource>();
+   
+   private static final String PRIMDOC = "primDoc";
+   private static final String PRIMDOC_LOC = "loc";
+   
+   private static final String DATASOURCE = "dataSource";
+   private static final String DATASOURCE_ID = "id";
+   private static final String DATASOURCE_TYPE = "mime"; // what kind of type text/video/pictures
+   private static final String DATASOURCE_LOCATION = "loc";
+   private static final String DATASOURCE_TIMEFRAME = "tf"; //timeframe - unittype (ms,frames, seconds);
+   private static final String DATASOURCE_FPS = "mfps"; // FPS in milliseconds
+   
     
    private static final String QUOTATION = "q";
    private static final String QUOTATION_ID = "id";
    private static final String QUOTATION_LABEL = "name";
+   private static final String QUOTATION_LOCATION = "loc";
    private static final String QUOTATION_CONTENT = "content";
    private static final String QUOTATION_CONTENT_P = "p";
    
@@ -120,16 +137,23 @@ public class ImporterAtlasTiXML implements FileImporter, LongTask{
    }
 
     private void importData(XMLStreamReader xmlReader) throws Exception {
+        
+        String primeDoc = "";
+        
         while (xmlReader.hasNext()) {
                 Integer eventType = xmlReader.next();
                 if (eventType.equals(XMLEvent.START_ELEMENT)) {
                     String name = xmlReader.getLocalName();
-                    if (QUOTATION.equalsIgnoreCase(name)) {
-                        readQuotation(xmlReader, null);
+                    if (PRIMDOC.equalsIgnoreCase(name)) {
+                        primeDoc = readPrimDocLocation(xmlReader);
+                    } else if (QUOTATION.equalsIgnoreCase(name)) {
+                        readQuotation(xmlReader, null, primeDoc);
                     } else if (CODE.equalsIgnoreCase(name)) {
                         readCode(xmlReader);
                     } else if (LINK.equalsIgnoreCase(name)) {
                         readLink(xmlReader);
+                    } else if (DATASOURCE.equalsIgnoreCase(name)){
+                        readDatasource(xmlReader, null);
                     }
                 } else if (eventType.equals(XMLStreamReader.END_ELEMENT)) {
                     String name = xmlReader.getLocalName();
@@ -143,9 +167,23 @@ public class ImporterAtlasTiXML implements FileImporter, LongTask{
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void readQuotation(XMLStreamReader reader,NodeDraft parent) throws Exception {
+    private String readPrimDocLocation(XMLStreamReader reader) throws Exception {
+       String loc = "";
+       for (int i = 0; i < reader.getAttributeCount(); i++) {
+            String attName = reader.getAttributeName(i).getLocalPart();
+            if (PRIMDOC_LOC.equalsIgnoreCase(attName)) {
+                loc = reader.getAttributeValue(i);
+            }
+        }
+
+       return loc;
+    }
+
+    private void readQuotation(XMLStreamReader reader,NodeDraft parent,String primDoc) throws Exception {
         String id = "";
         String label = "";
+        //String type = "";
+        String[] loc = null;
         String startDate = "";
         String endDate = "";
         String pid = "";
@@ -159,6 +197,17 @@ public class ImporterAtlasTiXML implements FileImporter, LongTask{
                 id = reader.getAttributeValue(i);
             } else if (QUOTATION_LABEL.equalsIgnoreCase(attName)) {
                 label = reader.getAttributeValue(i);
+            } else if (QUOTATION_LOCATION.equalsIgnoreCase(attName)) {
+                String location = reader.getAttributeValue(i);
+                String[] str;
+                str = location.split(",");
+                
+                loc = new String[str.length];
+                for (int j = 0; j< str.length; j++){
+                    loc[j] = str[j].split("@")[0];
+                    loc[j] = loc[j].split("!")[0];
+                    loc[j] = loc[j].trim();
+                }
             }
         }
 
@@ -175,6 +224,12 @@ public class ImporterAtlasTiXML implements FileImporter, LongTask{
         }
         node.setId(id);
         node.setLabel(label);
+        
+        if(loc.length > 0){
+            node = addStringAtributeToNodeTable(container,node,"locationStart","locationStart",loc[0], "int");
+            node = addStringAtributeToNodeTable(container,node,"locationEnd","locationEnd",loc[1], "int");
+        }
+        node = addStringAtributeToNodeTable(container,node,"primDoc","primDoc",primDoc, "string");
 
         //Parent
         if (parent != null) {
@@ -424,6 +479,102 @@ public class ImporterAtlasTiXML implements FileImporter, LongTask{
         
         
     }
+
+    private void readDatasource(XMLStreamReader reader,NodeDraft parent) throws Exception {
+        String startDate = "";
+        String endDate = "";
+        String pid = "";
+        boolean startOpen = false;
+        boolean endOpen = false;
+        
+        DataSource dataSource = new DataSource();
+
+        //Attributes
+        for (int i = 0; i < reader.getAttributeCount(); i++) {
+            String attName = reader.getAttributeName(i).getLocalPart();
+            if (DATASOURCE_ID.equalsIgnoreCase(attName)) {
+                dataSource.id = reader.getAttributeValue(i);
+            } else if (DATASOURCE_LOCATION.equalsIgnoreCase(attName)) {
+                dataSource.loc = reader.getAttributeValue(i);
+            } else if (DATASOURCE_TYPE.equalsIgnoreCase(attName)) {
+                dataSource.mime = reader.getAttributeValue(i);
+            } else if (DATASOURCE_TIMEFRAME.equalsIgnoreCase(attName)) {
+                dataSource.tf = reader.getAttributeValue(i);
+            } else if (DATASOURCE_FPS.equalsIgnoreCase(attName)) {
+                dataSource.mfps = Integer.parseInt(reader.getAttributeValue(i));
+            }
+        }
+
+        if (dataSource.id.isEmpty()) {
+            report.logIssue(new Issue("ID is empty", Issue.Level.SEVERE));
+            return;
+        }
+        
+        dataSourceList.add(dataSource);
+        
+    }
     
-     
+    private NodeDraft addStringAtributeToNodeTable(ContainerLoader container,NodeDraft node, String column_id, String column_title, String value, String type){
+        
+        AttributeType attributeType = AttributeType.STRING;
+        if (type.equalsIgnoreCase("boolean") || type.equalsIgnoreCase("bool")) {
+            attributeType = AttributeType.BOOLEAN;
+        } else if (type.equalsIgnoreCase("integer") || type.equalsIgnoreCase("int")) {
+            attributeType = AttributeType.INT;
+        } else if (type.equalsIgnoreCase("long")) {
+            attributeType = AttributeType.LONG;
+        } else if (type.equalsIgnoreCase("float")) {
+            attributeType = AttributeType.FLOAT;
+        } else if (type.equalsIgnoreCase("double")) {
+            attributeType = AttributeType.DOUBLE;
+        } else if (type.equalsIgnoreCase("string")) {
+            attributeType = AttributeType.STRING;
+        } else if (type.equalsIgnoreCase("bigdecimal")) {
+            attributeType = AttributeType.BIGDECIMAL;
+        } else if (type.equalsIgnoreCase("biginteger")) {
+            attributeType = AttributeType.BIGINTEGER;
+        } else if (type.equalsIgnoreCase("byte")) {
+            attributeType = AttributeType.BYTE;
+        } else if (type.equalsIgnoreCase("char")) {
+            attributeType = AttributeType.CHAR;
+        } else if (type.equalsIgnoreCase("short")) {
+            attributeType = AttributeType.SHORT;
+        } else if (type.equalsIgnoreCase("listboolean")) {
+            attributeType = AttributeType.LIST_BOOLEAN;
+        } else if (type.equalsIgnoreCase("listint")) {
+            attributeType = AttributeType.LIST_INTEGER;
+        } else if (type.equalsIgnoreCase("listlong")) {
+            attributeType = AttributeType.LIST_LONG;
+        } else if (type.equalsIgnoreCase("listfloat")) {
+            attributeType = AttributeType.LIST_FLOAT;
+        } else if (type.equalsIgnoreCase("listdouble")) {
+            attributeType = AttributeType.LIST_DOUBLE;
+        } else if (type.equalsIgnoreCase("liststring")) {
+            attributeType = AttributeType.LIST_STRING;
+        } else if (type.equalsIgnoreCase("listbigdecimal")) {
+            attributeType = AttributeType.LIST_BIGDECIMAL;
+        } else if (type.equalsIgnoreCase("listbiginteger")) {
+            attributeType = AttributeType.LIST_BIGINTEGER;
+        } else if (type.equalsIgnoreCase("listbyte")) {
+            attributeType = AttributeType.LIST_BYTE;
+        } else if (type.equalsIgnoreCase("listchar")) {
+            attributeType = AttributeType.LIST_CHARACTER;
+        } else if (type.equalsIgnoreCase("listshort")) {
+            attributeType = AttributeType.LIST_SHORT;
+        } else {
+            report.logIssue(new Issue("no type", Issue.Level.SEVERE));
+        }
+        
+        
+        if (container.getAttributeModel().getNodeTable().hasColumn(column_id) || container.getAttributeModel().getNodeTable().hasColumn(column_title)) {
+                //report.log(NbBundle.getMessage(ImporterAtlasTiXML.class, "importerGraphML_error_attributecolumn_exist", id));
+                AttributeColumn column = container.getAttributeModel().getNodeTable().getColumn(column_id);
+                node.addAttributeValue(column, value);
+                return node;
+            }
+        container.getAttributeModel().getNodeTable().addColumn(column_id, column_title, attributeType, AttributeOrigin.DATA, "");
+        AttributeColumn column = container.getAttributeModel().getNodeTable().getColumn(column_id);
+        node.addAttributeValue(column, value);
+        return node;
+    }
 }
